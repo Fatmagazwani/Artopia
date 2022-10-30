5d5acd4fb3d2673d5b09880a2c33ae6bec0e53b3
Milah Alfaqeeh <milahAlfaqeeh@gmail.com>
2022-10-30 10:13:08 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Milah Alfaqeeh  on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
