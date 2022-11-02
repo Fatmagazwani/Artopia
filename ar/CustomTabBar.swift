5a5419f3dc20d53594076bbde48cbb5e46f3daf7
Randa <Dr.randa.ma@gmail.com>
2022-11-02 13:19:17 +0300

    Initial Commit

---
+++ ar/CustomTabBar.swift
@@ -0,0 +1,56 @@
+//
+//  CustomTabBar.swift
+//  Artópia
+//
+//  Created by Fatma Gazwani on 05/04/1444 AH.
+//
+
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case house
+    case heart
+    case person
+    case cart
+}
+    struct CustomTabBar: View {
+        @Binding var selectedTab: Tab
+        private var fillImage: String {
+            selectedTab .rawValue + ".fill"
+        }
+        
+        
+        var body: some View {
+            VStack {
+                HStack {
+                    ForEach(Tab.allCases, id: \.rawValue) {tab in
+                        Spacer ()
+                        Image (systemName: selectedTab == tab ? fillImage : tab.rawValue)
+                            .scaleEffect (selectedTab == tab ? 1.25 : 1.0)
+                            .foregroundColor( selectedTab == tab ? Color("myblue") : .gray)
+                            .font(.system(size: 22))
+                            .onTapGesture {
+                                withAnimation (.easeIn(duration: 0.1)){
+                                    selectedTab = tab
+                                }
+                            }
+                        Spacer ()
+                    }
+                }
+                .frame(width: nil, height: 60)
+                .background(.thinMaterial)
+                .cornerRadius(30)
+                .padding()
+                
+                
+            }
+            .padding()
+        }
+    }
+    
+    struct CustomTabBar_Previews: PreviewProvider {
+        static var previews: some View {
+            CustomTabBar(selectedTab: .constant(.house))
+        }
+    }
+
